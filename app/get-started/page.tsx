dc70bc096dba6f1c91eb0acee80b97e98e16614a
Takhiyuddin 'Afif <afiftqydns@gmail.com>
2025-06-03 16:07:40 +0700

    Updated page.tsx

--- app/get-started/page.tsx
+++ app/get-started/page.tsx
@@ -92,9 +92,7 @@ export default function GetStarted() {
                 <div>
                   <h3 className="text-lg font-semibold mb-1">Location</h3>
                   <p className="text-gray-600">
-                    Jalan Raya Randusari-Klaten KM 0.4<br />
-                    Cemoro 07/02, Dusun III, Randusari<br />
-                    Boyolali, Central Java 57372
+                    Boyolali, Central Java
                   </p>
                 </div>
               </div>
